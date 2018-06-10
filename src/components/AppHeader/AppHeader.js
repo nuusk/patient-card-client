778873d5971d85d8b5ffed06854da23f43ff1fa9
Piotr Ptak <robakzdobywca@gmail.com>
2018-06-10 11:21:57 +0200

    Create basic app structure :contruction: distinction between components, containers and views; :art: create AppHeader component;

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppHeader.css';
+
+const AppHeader = () => {
+  return (
+    <div className="AppHeader">
+      Patient card
+    </div>
+  );
+};
+
+export default AppHeader;
